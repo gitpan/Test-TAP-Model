50f078cb2a18a48163bc023ee82633ab244766ee
יובל קוג'מן (Yuval Kogman) <nothingmuch@woobling.org>
2008-06-22 03:58:47 +0000

    Import of NUFFIN/Test-TAP-Model-0.10 from CPAN.
    gitpan-cpan-distribution: Test-TAP-Model
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         NUFFIN/Test-TAP-Model-0.10.tar.gz
    gitpan-cpan-author:       NUFFIN
    gitpan-cpan-maturity:     released

--- lib/Test/TAP/Model.pm
+++ lib/Test/TAP/Model.pm
@@ -10,7 +10,7 @@ use Test::TAP::Model::File;
 
 use List::Util qw/sum/;
 
-our $VERSION = "0.09";
+our $VERSION = "0.10";
 
 # callback handlers
 sub _handle_bailout {
@@ -230,8 +230,7 @@ __END__
 
 =head1 NAME
 
-Test::TAP::Model - Accessible (queryable, serializable object) result collector
-for L<Test::Harness::Straps> runs.
+Test::TAP::Model - DEPRECATED Use L<TAP::Harness>, L<TAP::Formatter::HTML>
 
 =head1 SYNOPSIS
 
